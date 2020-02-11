020b49ddd30fe24daeb5e33e89da4144cc950e92
Elizaveta2204 <bashckovaliza@yandex.ru>
2020-02-11 21:04:16 +0300

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Vector2d.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Vector2d a = { 6, 15 };
+	Vector2d b = { 4, 10 };
+	cout << "Координаты вектора а "; a.print();
+	cout << "Координаты вектора b ";  b.print();
+	cout << "Скалярное произведение: " << a.scalarMult(b) << endl;
+	cout << "Тангенс между двумя векторами: " << a.tg(b) << endl;
+}
